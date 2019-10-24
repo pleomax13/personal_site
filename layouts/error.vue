5f8104ece5e658ed7c878264704c7d30f82ac252
pleomax13 <dimonoid9595@gmail.com>
2019-10-24 23:15:52 +0300

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1>{{ error.statusCode }}</h1>
+    <h3>{{ error.message }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  margin: auto;
+}
+
+h1 {
+  font-size: 4rem;
+  text-align: center;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
